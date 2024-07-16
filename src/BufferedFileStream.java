13e83781bfc1d464849ef98c9e2c609ae62743dc
Bernardo Dauber <be.daubervieira@gmail.com>
2024-07-16 11:13:30 -0300

    Block try with resources

--- src/BufferedFileStream.java
+++ src/BufferedFileStream.java
@@ -7,40 +7,65 @@ public class BufferedFileStream {
     public static void main(String[] args) {
 
        //Método "sujo" de abrir/ler arquivos
+//        String path = "E:\\Java\\Arquivos\\src\\files\\nome.txt";
+//        FileReader fr = null;
+//        BufferedReader br = null;
+//
+//
+//        try{
+//            fr = new FileReader(path);
+//            br = new BufferedReader(fr);
+//
+//            String line = br.readLine();
+//
+//            while(line != null){
+//                System.out.println(line);
+//                line = br.readLine();
+//            }
+//        }
+//        catch (IOException e ){
+//            System.out.println("Error:" + e.getMessage());
+//        }
+//        finally {
+//
+//            try {
+//                if (br != null) {
+//                    br.close();
+//                }
+//                if (fr != null) {
+//                    fr.close();
+//                }
+//
+//            }
+//            catch(IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+
+    //Usando Block try with Resources:
+        //Método "simplificado"
         String path = "E:\\Java\\Arquivos\\src\\files\\nome.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-
-
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
-            while(line != null){
+            while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
-        }
-        catch (IOException e ){
+        } catch (IOException e) {
             System.out.println("Error:" + e.getMessage());
         }
-        finally {
 
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
+    /*
+    Bloco try-with-resources:
+        • É um bloco try que declara um ou mais recursos, e garante que esses
+        recursos serão fechados ao final do bloco
 
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-        }
+    */
+
+
+
 
     }
 
